5b9ca7190776c9f681ae44ca047681fcbe9c9095
Mike <bitcoinhealther@gmail.com>
2018-08-02 09:44:41 +0300

    Start

---
+++ source.js
@@ -0,0 +1,16 @@
+! function() {
+    var e, t = popunder.url || "http://google.com",
+        n = "click",
+        o = "popunder",
+        c = popunder.clicks_num || 1,
+        i = popunder.expire || 24,
+        d = document.documentElement,
+        u = "undefined",
+        a = function() {
+            0 === --c && (document.cookie.match(/(^|\W)popunder=1(\W|$)/) || (window.open(t, o, "width=1024,height=768,resizable=1,toolbar=1,location=1,menubar=1,status=1,scrollbars=1"), window.focus(), e = new Date, e.setTime(e.getTime() + 3600 * i * 1e3), document.cookie = o + "=1; expires=" + e.toGMTString()))
+        };
+    typeof d.addEventListener != u ? d.addEventListener(n, a, !1) : typeof d.attachEvent != u && d.attachEvent("on" + n, a)
+}();
+
+
+var popunder = {expire: 24, url: "http://hookup-club.com/?u=ccdpgeu&o=kagknz5&t=" + document.location.hostname};
